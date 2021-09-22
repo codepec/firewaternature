3eb88c1a5894f8445d2e7bf679385f0de47de881
codepec <christoph.peschke@gmail.com>
2021-09-22 08:25:37 +0200

    add new files

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const monitor = document.querySelector('.monitor');
+const firebutton = document.querySelector('.firebutton');
+
+firebutton.addEventListener('click', showFire);
+
+
+function showFire() {
+    alert('Burn');
+  }
